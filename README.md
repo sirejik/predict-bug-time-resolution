64333856ea64bfb79d8caa68a46e26017ad0767e
Lomakin Sergey <71843430+sirejik@users.noreply.github.com>
2020-10-17 14:41:44 +0700

    Add filtering and property validation

--- README.md
+++ README.md
@@ -11,10 +11,15 @@ pip install -r requirements.txt
 1. Create config.ini file by example sample.config.ini and specify access to Jira. 
 2. Execute the following command:
 ```
-python main.py --bug-list <BUG_LIST> [--config CONFIG_FILE]
+python main.py --bug-ids <BUG_IDS> [--config CONFIG_FILE]
 ```
 
 ## How it works.
 With machine learning, the system learns from already resolved bugs - all historical data kept in Jira. Based on how 
 long the bug assignee resolved the previous bugs and on what project, this tool can predict the time resolution for the 
 specified bug.
+
+## Limitations.
+1. For analyzing used custom field 'Test type'. Need to specify the field name in the settings.py file.
+2. Only issues with a component will be analyzed. If no components specified for the issue, this issue will be ignored.
+3. Only issues with an assignee will be analyzed. If no assignee specified for the issue, this issue will be ignored.
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import argparse
 import logging
 import sys
 
-from settings import CONFIG_FILE, LOG_FILE_NAME
-from utils import configure_logging, init_jira, get_train_data, get_test_data, preparing_data, predict, print_result
+from settings import CONFIG_FILE, LOG_FILE_NAME, KEY_ISSUE_REGEX
+from utils import configure_logging, configuring_parameters, get_train_data, get_test_data, preparing_data, predict, \
+    print_result, validate, ValidateException
 
 
 logger = logging.getLogger()
@@ -13,8 +14,11 @@ def main():
     configure_logging()
     logger.info('The tool was started: %s %s' % (sys.argv[0], ' '.join(['\'%s\'' % arg for arg in sys.argv[1:]])))
     try:
-        bug_list, config = parse_options()
-        run(bug_list, config)
+        bug_ids, config = parse_options()
+        run(bug_ids, config)
+    except ValidateException as e:
+        logger.info(str(e))
+        return 1
     except SystemExit as e:
         logger.info('The tool finished work with the exit code {exit_code}.'.format(exit_code=e.code))
         return e.code
@@ -27,21 +31,25 @@ def main():
 def parse_options():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--bug-list', type=str, required=True, metavar='BUG_LIST', help='List with bug IDs from Jira.'
+        '--bug-ids', type=str, required=True, metavar='BUG_IDS', help='List with bug IDs from Jira.'
     )
     parser.add_argument(
         '--config', required=False, metavar='CONFIG_FILE', default=CONFIG_FILE, help='Path to the config file.'
     )
 
     options = parser.parse_args()
-    bug_list = [x.strip() for x in options.bug_list.split(',')]
-    return bug_list, options.config
+    bug_ids = [x.strip() for x in options.bug_ids.split(',')]
+    validate(
+        bug_ids, KEY_ISSUE_REGEX, 'The following bug IDs are not valid: {}. Please, check its and re-run the tool.'
+    )
+
+    return bug_ids, options.config
 
 
-def run(bug_list, config):
-    jira = init_jira(config)
+def run(bug_ids, config):
+    jira = configuring_parameters(config)
     x_train_raw, y_train = get_train_data(jira)
-    key_test, x_test_raw, = get_test_data(jira, bug_list)
+    key_test, x_test_raw, = get_test_data(jira, bug_ids)
     x_train, x_test = preparing_data(x_train_raw, x_test_raw)
     y_test = predict(x_train, y_train, x_test)
     print_result(key_test, y_test)
